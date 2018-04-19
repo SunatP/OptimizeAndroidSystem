417ec1b656daef28a1c2d610f27b407e3eb51758
IZIMI\Sunat.P <galaxysuperplus@gmail.com>
2018-04-19 13:34:56 +0700

    add comment

--- For Android/For Global User/Audio Module/50V4A-edit.sh
+++ For Android/For Global User/Audio Module/50V4A-edit.sh
@@ -1,3 +1,4 @@
 #! /system/bin/sh
 supolicy --live "allow mediaserver mediaserver_tmpfs:file { read write execute };"
 supolicy --live "attradd rild mlstrustedsubject"
+# use this with experimental add into ZIP file V4A su-Policy and rename to 50viper
